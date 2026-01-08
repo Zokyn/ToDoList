0b283f8f37c52fb5b5eb714ec8b61a0a1aad69fe
Zokyn <gustavo.casaes@mail.uft.edu.br>
2026-01-08 00:49:36 -0300

    feat: adicionado scss ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   const [checked, setChecked] = useState(false);
@@ -14,29 +14,36 @@ function App() {
   return (
     <div id="note-panel">
       <h1>{date}</h1>
-      <span>Afazeres de hoje</span>
-      <div className="card">
-        <button
-          className={`to-do-item ${checked && "checked"}`}
-          onClick={() => setChecked((checked) => !checked)}
-        >
-          <input type="checkbox" checked={checked} />
-          <span>Tirar o Lixo</span>
-        </button>
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <>
+        <span>Afazeres de hoje</span>
+        <div className="to-do-list">
+          <button
+            className={`to-do-item ${checked && "checked"}`}
+            onClick={() => setChecked((checked) => !checked)}
+          >
+            <input type="checkbox" checked={checked} />
+            <span>Tirar o Lixo</span>
+          </button>
+          <button
+            className={`to-do-item new-one`}
+            onClick={() => setChecked((checked) => !checked)}
+          >
+            <input type="checkbox" checked={false} />
+            <span>Novo item</span>
+          </button>
+        </div>
+      </>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+      </p>
     </div>
   );
 }
