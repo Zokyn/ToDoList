0c7b265310f67d0cb381e769260ca86e49952c35
Zokyn <gustavo.casaes@mail.uft.edu.br>
2026-01-08 15:29:06 -0300

    bugfix: corrigido erros para permitir build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useRef, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from "react";
 import "./App.scss";
 import ToDoItem, { ToDo } from "./components/ToDoItem";
 import AddToDo from "./components/AddToDo";
